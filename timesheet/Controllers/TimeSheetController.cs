6526902f498537958a38643ade5cec50755c135c
Patrick Desmarais <patrick.desm@gmail.com>
2015-01-26 23:17:33 +0000

    Simple text response to /timesheets

---
+++ timesheet/Controllers/TimeSheetController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace timesheet.Controllers
+{
+    public class TimeSheetController : Controller
+    {
+        // GET: TimeSheet
+        public string /*ActionResult*/ Index()
+        {
+            return "This is my default response!"; View();
+        }
+    }
+}
