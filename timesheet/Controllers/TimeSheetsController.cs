e12eb43e628d6b1fc8ed3f9e0b30e301d1a96289
Patrick Desmarais <patrick.desm@gmail.com>
2015-02-02 21:44:10 +0000

    Added two templates for the Detail and List views

--- timesheet/Controllers/TimeSheetsController.cs
+++ timesheet/Controllers/TimeSheetsController.cs
@@ -11,8 +11,8 @@ namespace timesheet.Controllers
         int ASCENDING_ORDER = 0;
         int DESCENDING_ORDER = 1;
 
-        // GET /TimeSheets
-        public string /*ActionResult*/ Index(int ID = 0, string sortOrder = null, int fromIndex = 0, int range = 20)
+        // GET /TimeSheets...
+        public ActionResult Index(int ID = 0, string sortOrder = null, int fromIndex = 0, int range = 20)
         {
             if (ID > 0)
             {
@@ -23,14 +23,16 @@ namespace timesheet.Controllers
             return ShowList(normalizedSortOrder, fromIndex, range);
         }
 
-        private string ShowDetail(int ID)
+        // GET /TimeSheets/{ID}
+        private ActionResult ShowDetail(int ID)
         {
-            return HttpUtility.HtmlEncode("Time sheet #" + ID);
+            return View("Detail");
         }
 
-        private string ShowList(int sortOrder, int fromIndex, int range)
+        // GET /TimeSheets
+        private ActionResult ShowList(int sortOrder, int fromIndex, int range)
         {
-            return HttpUtility.HtmlEncode("Time sheets ordered by " + sortOrder + " (0 ascending, 1 descending) in batch of " + range + " entries, starting from index " + fromIndex); // View();
+            return View("Index");
         }
     }
 }
